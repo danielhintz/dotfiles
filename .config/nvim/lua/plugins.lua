fd3ce932f4d8a10400b027f2ca458ed02e3a85e0
dev <dev@kraken.danielhintz.xyz>
2023-04-19 21:38:06 -0400

    Syncing

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -8,6 +8,30 @@ return require('packer').startup(function(use)
     use 'L3MON4D3/LuaSnip'
     use 'hrsh7th/cmp-nvim-lsp'
     use 'saadparwaiz1/cmp_luasnip'
+    use 'mbbill/undotree'
+    use 'zchee/vim-flatbuffers'
+    use 'yanick/nvim-taskwarrior'
     use 'michalbachowski/vim-wombat256mod'
-end)
+    use 'integralist/vim-mypy'
+    use 'easymotion/vim-easymotion'
+    use 'bkad/CamelCaseMotion'
+    use 'vim-scripts/taglist.vim'
+    use 'ntpeters/vim-better-whitespace'
+    use 'preservim/nerdtree'
+    use 'mg979/vim-visual-multi'
+    use 'google/vim-maktaba'
+    use 'bazelbuild/vim-bazel'
+    use 'rhysd/conflict-marker.vim'
+    use 'github/copilot.vim'
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
 
+    use {
+        'tanvirtin/vgit.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim'
+        }
+    }
+end)
